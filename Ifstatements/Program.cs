5b3d6bf6180fa3089a991b8b72f8eb89ee02f443
Opilane TTHK <opilane@tthk.ee>
2021-09-07 11:41:31 +0300

    If Statements

---
+++ Ifstatements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ifstatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //programm küsib kasutaja vanust
+            //kui kasutajaon vanem kui 18 siis konsoolis kuvatakse "oled piisavalt vana"
+            //kui kasutaja on noorem, kui 18 siis konsoolis kuvatakse "oled liiga noor"
+            //kui kasutaja 18, siis konsoolis kuvatakse "oledki 18"
+            Console.WriteLine("kui vana sa oled?");
+
+            int Age = Int32.Parse(Console.ReadLine());
+
+            if (Age > 18)
+            {
+                Console.WriteLine("oled piisavalt vana");
+            }
+            else if (Age < 18)
+                Console.WriteLine("oled liiga noor");
+           
+            else
+            {
+                Console.WriteLine("oledki 18");
+
+            }
+            Console.WriteLine("Ilusta päeva!");
+
+
+          
+               
+            
+            
+            
+
+
+
+
+
+            
+
+        }
+    }
+    
+}
